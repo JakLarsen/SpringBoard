495e97ddb06feac142b5ddfb2b2c156c7fd4e845
= <jakelarsendk@gmail.com>
2021-03-18 17:48:04 -0500

    madlibs done

--- 19.flask/19.2/madlibs/app.py
+++ 19.flask/19.2/madlibs/app.py
@@ -1,6 +1,14 @@
 from flask import Flask, request, render_template
+from random import randint, choice, sample
+from stories import Story
 
 app = Flask(__name__)
+
+#Story instance to use to show the forms are not hardcoded and can be used with different prompts
+myStory = Story(
+    ["animal", "noun", "verb", "adjective", "plural_noun"],
+    """One {adjective} day, a little {animal} and his {plural_noun} went to the {noun} to {verb}."""
+)
 
 @app.route('/')
 def show_home():
@@ -8,11 +16,16 @@ def show_home():
 
 @app.route('/form')
 def show_form():
-    return render_template('form.html')
+    story_words = myStory.prompts
+    return render_template('form.html', story_words = story_words)
 
 @app.route('/story')
 def show_story():
-    return render_template('story.html')
+    text = myStory.generate(request.args)
+    return render_template('story.html', text = text)
 
 
+# Did the entire thing except for line 24, but do NOT understand how to set something like this up for myself
+# Where are the query strings being defined on form submit?????
+# What is request.args? Why can I not capture it in a variable? Yet it works within this function?
 
