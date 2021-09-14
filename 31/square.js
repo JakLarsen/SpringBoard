74d647040ddea34f4709345a0ea95dcab1b0d398
= <jakelarsendk@gmail.com>
2021-09-14 09:01:58 -0500

    31.5.4 done

--- 31/square.js
+++ 31/square.js
@@ -3,5 +3,11 @@
 function squareMe(x){
     return x*x
 }
+function cubeMe(x){
+    return Math.pow(x,3)
+}
 
-module.exports = {squareMe}
+
+
+
+module.exports = {squareMe, cubeMe}
--- 31/square.test.js
+++ 31/square.test.js
@@ -1,6 +1,25 @@
-const {squareMe} = require('./square.js')
+const {squareMe, cubeMe} = require('./square.js')
 
-test('Square should square a number', function(){
-    const res = squareMe(3)
-    expect(res).toEqual(9)
+
+describe ('Square Function', function(){
+    test('Square should square a number', function(){
+        const res = squareMe(3)
+        expect(res).toEqual(9)
+    })
+
+    test('Square should square negtative numbers', function(){
+        const res = squareMe(-9)
+        expect(res).toEqual(81)
+    })
+})
+
+describe ('Cube Function', function(){
+    test('Cube should cube a positive number', function(){
+        const res = cubeMe(3)
+        expect(res).toEqual(27)
+    })
+    test('Cube should give -cube of a negative number', function(){
+        const res = cubeMe(-2)
+        expect(res).toEqual(-8)
+    })
 })
