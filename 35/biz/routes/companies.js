802f2917a3b7dcf1fbf50c2d095026324f155ec3
= <jakelarsendk@gmail.com>
2021-09-17 11:12:16 -0500

    companies CRUD routes set up

--- 35/biz/routes/companies.js
+++ 35/biz/routes/companies.js
@@ -7,7 +7,73 @@ const app = require('../app')
 
 
 router.get('/', async function(req, res, next){
-   
+    try{
+        const results = await db.query(
+                'select * from companies'
+            )
+        return res.status(200).json({companies: results.rows})
+    }
+    catch(e){
+        next(e)
+    } 
+})
+router.get('/:code', async function(req, res, next){
+    const {code} = req.params
+    try{
+        const results = await db.query(
+                'select * from companies where code=$1', [code]
+            )
+        if (results.rows.length == 0){
+            throw new ExpressError('Company code not found.', 404)
+        }
+        return res.status(200).json({companies: results.rows})
+    }
+    catch(e){
+        next(e)
+    } 
+})
+router.post('/', async (req, res, next)=>{
+    const {code, name, description} = req.body
+    try{
+        const results = await db.query(
+            `insert into companies (code, name, description) values ($1, $2, $3) 
+            returning code, name, description`, [code, name, description]
+        )
+        return res.status(201).json({company: results.rows[0]})
+    }
+    catch(e){
+        next(e)
+    }
+})
+
+router.patch('/:code', async (req,res,next)=>{
+    try{
+        const {code} = req.params
+        const {name, description} = req.body
+        const results = await db.query(
+            `UPDATE companies SET name=$2, description=$3 WHERE code=$1 RETURNING code, name, description`, [code, name, description ]
+        )
+        if (results.rows.length == 0){
+            throw new ExpressError("Company code not found", 404)
+        }
+        return res.status(200).send({company: results.rows[0]})
+    }
+    catch(e){
+        return next(e)
+    }
+})
+
+router.delete('/:code', async (req,res,next)=>{
+    try{
+        const {code} = req.params
+        const results = await db.query(
+            `delete from companies where code=$1`, [code]
+        )
+        return res.send({msg : "DELETED!" })
+    }
+    catch(e){
+        return next(e)
+    }
 })
 
 
