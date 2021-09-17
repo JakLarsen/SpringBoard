cfa80e4ab28886a02a3b88717210561bc15cf11e
= <jakelarsendk@gmail.com>
2021-09-17 10:54:20 -0500

    biztime up and running

---
+++ 35/biz/app.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const ExpressError = require('./expressError')
+
+const app = express()
+app.use(express.json())
+
+const companies = require('./routes/companies')
+app.use("/companies", companies)
+
+
+
+
+
+
+
+
+
+
+//If nothing else runs, generic 404
+app.use((req,res,next)=>{
+    const e = new ExpressError("Page not found", 404)
+    next(e)
+})
+
+//Otherwise we use an error handler
+app.use((error, req, res, next)=>{
+    let status = error.status || 500
+    let msg =  error.msg
+    res.status(status).send({error: {msg: msg, status: status}})
+})
+
+module.exports = app
---
+++ 35/biz/routes/companies.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const ExpressError = require('../expressError')
+const router = new express.Router()
+const db = require('../db')
+const app = require('../app')
+
+
+
+router.get('/', async function(req, res, next){
+   
+})
+
+
+
+module.exports = router
