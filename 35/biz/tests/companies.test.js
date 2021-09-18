3a03e178eedaa4c121796bafd2a6774b5ac2b1c5
= <jakelarsendk@gmail.com>
2021-09-18 07:55:20 -0500

    first tests successful

--- 35/biz/tests/companies.test.js
+++ 35/biz/tests/companies.test.js
@@ -1,4 +1,39 @@
 process.env.NODE_ENV === "test"
+
 const request = require('supertest')
 const app = require('../app')
 const db = require('../db')
+
+
+
+                    // SETUP 
+
+
+
+let testCompany;
+beforeEach(async ()=>{
+    const result = await db.query(
+        `insert into companies (code, name, description) values ('test', 'test_company', 'a_test')
+        returning code, name, description`
+    )
+    testCompany = result.rows[0]
+})
+afterEach(async ()=>{
+    await db.query(`delete from companies`)
+})
+afterAll(async ()=>{
+    await db.end()
+})
+
+
+
+                    // BASIC ROUTE TESTS
+
+
+                    
+describe("Hope this test is working", ()=>{
+    test("First test", ()=>{
+        console.log(testCompany)
+        expect(1).toBe(1)
+    })
+})
--- 35/biz/tests/invoices.test.js
+++ 35/biz/tests/invoices.test.js
@@ -1,4 +1,45 @@
 process.env.NODE_ENV === "test"
+
 const request = require('supertest')
 const app = require('../app')
 const db = require('../db')
+
+
+
+                    // SETUP 
+
+
+
+let testInvoice;
+let testCompany
+beforeEach(async ()=>{
+    const compResult = await db.query(
+        `insert into companies (code, name, description) values ('test', 'test_company', 'a_test')
+        returning code, name, description`
+    )
+    const invResult = await db.query(
+        `insert into invoices (comp_code, amt) values ('test', '150')
+        returning id, comp_code, amt, paid, add_date, paid_date`
+    )
+    testInvoice = invResult.rows[0]
+})
+afterEach(async ()=>{
+    await db.query(`delete from invoices`)
+})
+afterAll(async ()=>{
+    await db.end()
+})
+
+
+
+                    // BASIC ROUTE TESTS
+
+
+                    
+describe("Hope this test is working", ()=>{
+    test("First test", ()=>{
+        console.log(testInvoice)
+        expect(1).toBe(1)
+    })
+})
+
