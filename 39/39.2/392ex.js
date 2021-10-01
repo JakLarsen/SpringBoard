25e2ddbfc75a609637ae8a77fc6011f8c943c59a
= <jakelarsendk@gmail.com>
2021-10-01 11:06:58 -0500

    39.2ex P2 complete

--- 39/39.2/392ex.js
+++ 39/39.2/392ex.js
@@ -7,10 +7,21 @@ const FirstComponent = ({msg}) =>{
     return (
         <h1>{msg}</h1>
     )
-}
+} 
+
 const NamedComponent = ({name}) =>{
     return (
         <p>My name is {name}.</p>
+    )
+}
+
+const Tweet = ({username, name, date, msg}) =>{
+    return(
+        <div>
+            <h1>{username} {name}</h1>
+            <p>{msg}</p>
+            <p>{date}</p>
+        </div>
     )
 }
 
@@ -20,6 +31,9 @@ const App = () =>{
         <div>
             <FirstComponent msg="My very first component"/>
             <NamedComponent name="Jake"/>
+            <Tweet username="JakeLarsen11" name="Jake" date="10/1/21" msg="My first tweet"/>
+            <Tweet username="MakeLarsen11" name="Jake" date="10/1/21" msg="My first tweet"/>
+            <Tweet username="CakeLarsen11" name="Jake" date="10/1/21" msg="My first tweet"/>
         </div>
     )
 }
