baa66f4de5def844a7377d8dad81f8eccacdbc2e
= <jakelarsendk@gmail.com>
2021-10-01 11:16:27 -0500

    39.2ex Complete

--- 39/39.2/392ex.js
+++ 39/39.2/392ex.js
@@ -25,6 +25,19 @@ const Tweet = ({username, name, date, msg}) =>{
     )
 }
 
+const Person = (props) =>{
+    return (
+        <div>
+            <p>Learn some information about {props.name}:</p>
+            <h3>Age: {props.age} - {props.age >= 18 ? "Please go vote." : "You must be 18."}</h3>
+            <h3>Name: {props.name} - Shortened: {props.name.length >= 6 ? props.name.substring(0,6) : props.name}</h3>
+            <ul>
+                {props.hobbies.map(hobby=><li>{hobby}</li>)}
+            </ul>
+        </div>
+    )
+}
+
 
 const App = () =>{
     return(
@@ -34,6 +47,9 @@ const App = () =>{
             <Tweet username="JakeLarsen11" name="Jake" date="10/1/21" msg="My first tweet"/>
             <Tweet username="MakeLarsen11" name="Jake" date="10/1/21" msg="My first tweet"/>
             <Tweet username="CakeLarsen11" name="Jake" date="10/1/21" msg="My first tweet"/>
+            <Person name="Jake" age="28" hobbies={['Piano', 'Skydiving', 'Volunteering']}/>
+            <Person name="CupJakes" age="17" hobbies={['School', 'Soccer', 'Comedy']}/>
+            <Person name="MichaelangeJake" age="747" hobbies={['Paint', 'Sculpt', 'Stare at Ceilings']}/>
         </div>
     )
 }
