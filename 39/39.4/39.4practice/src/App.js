bf1993bc2e72a63ba4ad1505a0b9d64a060f1704
= <jakelarsendk@gmail.com>
2021-10-20 07:55:59 -0500

    update

--- 39/39.4/39.4practice/src/App.js
+++ 39/39.4/39.4practice/src/App.js
@@ -3,17 +3,28 @@ import {Clicker, MouseOver, Scroll } from './Clicker'
 import ButtonGroup from './ButtonGroup';
 import Counter from './Counter'
 import NumberGame from './NumberGame';
+import FixedComponent from './FixedComponent';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>Following Along</h1>
+//       <Clicker/>
+//       <MouseOver/>
+//       <Scroll/>
+//       <ButtonGroup/>
+//       <Counter/>
+//       <NumberGame/>
+//     </div>
+//   );
+// }
 
 function App() {
   return (
     <div className="App">
       <h1>Following Along</h1>
-      <Clicker/>
-      <MouseOver/>
-      <Scroll/>
-      <ButtonGroup/>
       <Counter/>
-      <NumberGame/>
+      <FixedComponent/>
     </div>
   );
 }
--- 39/39.4/39.4practice/src/Counter.test.js
+++ 39/39.4/39.4practice/src/Counter.test.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import Counter from './Counter';
 
 
 
 
 test("renders without crashing", ()=>{
-  render(<App/>)
+  render(<Counter/>)
 })
