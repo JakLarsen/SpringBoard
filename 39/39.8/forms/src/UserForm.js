7c51c53b2feff5a37ee81da278891f9dfcc1f0ef
= <jakelarsendk@gmail.com>
2021-10-22 08:35:43 -0500

    39.8.1-2 done

---
+++ 39/39.8/forms/src/UserForm.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const UserForm = () => {
+
+    return (
+        <form>
+            <input type="text" placeholder="username"/>
+            <button>Add</button>
+
+        </form>
+    )
+}
+
+export default UserForm
