b2845c48ac58cea7dc30e724a4eaae5490b0013e
= <jakelarsendk@gmail.com>
2021-10-22 08:48:43 -0500

    39.8.3-5 done

--- 39/39.8/forms/src/UserForm.js
+++ 39/39.8/forms/src/UserForm.js
@@ -1,12 +1,28 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const UserForm = () => {
 
-    return (
-        <form>
-            <input type="text" placeholder="username"/>
-            <button>Add</button>
+    const [username, setUsername] = useState("")
 
+    //Can add live validation if you use controlled components
+    const handleChange = (e) => {
+        console.log('handleChange()')
+        console.log(e.target.value)
+        setUsername(e.target.value)
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(`Created user: ${username}`)
+        setUsername("")
+    }
+
+    return (
+        //Submit form (can also hit enter for this one) or hit button for same functionality
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="username">Username</label>
+            <input id="username" type="text" placeholder="username" value={username} onChange={handleChange}/>
+            <button>Add</button>
         </form>
     )
 }
