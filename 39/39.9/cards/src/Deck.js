58f1c16f54b095c6e42c70463501ac24708c354c
= <jakelarsendk@gmail.com>
2021-10-25 12:27:39 -0500

    39.9 ex card ex done

--- 39/39.9/cards/src/Deck.js
+++ 39/39.9/cards/src/Deck.js
@@ -16,6 +16,9 @@ const Deck = () => {
     const [cards, setCards] = useState([])
     const [deck, setDeck] = useState(`http://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1`)
     const [deckID, setDeckID] = useState("")
+    const [isDrawing, setIsDrawing] = useState(false)
+    const timerID = useRef()
+    const [startStop, setStartStop] = useState('Start')
 
     const createDeck = async () => {
         let ourDeck = await axios.get(deck)
@@ -29,7 +32,7 @@ const Deck = () => {
     const draw = async () => {
 
         let res = await axios.get(`http://deckofcardsapi.com/api/deck/${deckID}/draw/?count=1`)
-
+        console.log(res.data.remaining)
         if (res.data.remaining == 0){
             createDeck()
             setCards([])
@@ -40,16 +43,40 @@ const Deck = () => {
             let newCard = {value: ourCard.value, suit: ourCard.suit} 
             setCards(cards=>[...cards, newCard])     
         }
+
     }
 
-    const timedDraw = async () =>{
-        setInterval(draw, 1000)
+    const start = () =>{
+        timerID.current = setInterval(draw, 1000)
+        return () => {
+            clearInterval(timerID.current)
+        }
     }
+
+    const stop = () => {
+        clearInterval(timerID.current)
+    }
+
+    const handleStartStop = () => {
+        if (isDrawing){
+            setIsDrawing(!isDrawing)
+            setStartStop('Start')
+            stop()
+        }
+        else{
+            setIsDrawing(!isDrawing)
+            setStartStop('Stop')
+            start()
+        }
+    }
+
 
     return (
         <div className="Deck">
             <button onClick={draw}>Draw</button>
-            <button onClick={timedDraw}>Start/Stop</button>
+            <button onClick={start}>Start</button>
+            <button onClick={stop}>Stop</button>
+            <button onClick={handleStartStop}>Dual: {startStop}</button>
             <div className="Deck-card-con">
                 {cards.map(card => (
                     <Card key={uuid()}value={card.value} suit={card.suit}/>
