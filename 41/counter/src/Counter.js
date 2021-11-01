00f834082f989b91aafbc1425e8d06a78dd373f6
= <jakelarsendk@gmail.com>
2021-11-01 07:50:55 -0500

    41.3.3-4 done

--- 41/counter/src/Counter.js
+++ 41/counter/src/Counter.js
@@ -21,16 +21,34 @@ class Counter extends React.Component{
 
     constructor(props) {
         super(props);
-        console.log(this.props)
+        //ONE STATE for each component instead of separated state
+        this.state = {
+            count: 0,
+            isHiding: false
+        }
+        this.increment = this.increment.bind(this)
+        this.decrement = this.decrement.bind(this)
     }
+
+    increment () {
+        this.setState({count: this.state.count+1})
+    }
+    decrement () {
+        this.setState({count: this.state.count-1})
+    }
+
 
 
     render(){
         const {color} = this.props
+        const {count} = this.state
         return (
             <div>
-                <h1>Count: </h1>
-                <button style={{color: color}}>Add</button>
+                <h1 style={{color: color}}>Count: {count} </h1>
+                <button onClick={()=>this.setState({count: count+1})}>Add</button>
+                <button onClick={()=>this.setState({count: count-1})}>Subtr</button>
+                <button onClick={this.increment}>Add</button>
+                <button onClick={this.decrement}>Subtr</button>
             </div> 
         )
     }
